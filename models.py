763f616ee0ab6bb303317275596d36303f8b163e
JianJiang_Omen <jian.jiang@kcl.ac.uk>
2022-03-30 10:03:46 +0100

    adaptive layer-wise fnemb

--- models.py
+++ models.py
@@ -7,7 +7,8 @@ from torch.nn.parameter import Parameter
 from einops import rearrange, repeat
 from functools import partial, wraps
 
-from quantizer_v3 import Quantizer
+# from quantizer_v3 import Quantizer
+from classifiers import Quantizer
 from torchvision.models import resnet18, mobilenet_v2
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
@@ -54,9 +55,10 @@ class QuantHelper(nn.Module):
 class FeatureQuantizer(QuantHelper):
     def __init__(self, dim, n_emb, decay=0.99):
         super().__init__()
+        self.use_quant = False
         self.target = QUANT_TYPE.FEATURE
         self.quantizer = Quantizer(dim, n_emb, decay)
-        self.diff = 0
+        self.diff = torch.Tensor([0]).cuda()
         
     def normal_forward(self, x):
         return x
@@ -72,6 +74,7 @@ class FeatureQuantizer(QuantHelper):
 class QuantConv_DW(QuantHelper):
     def __init__(self, prod_dw_in_planes, in_planes, kernel_size=3, stride=1, padding=0):
         super().__init__()
+        self.use_quant = False
         self.kernel_size = kernel_size
         self.prod_dw_in_planes = prod_dw_in_planes
         self.in_planes = in_planes
@@ -79,21 +82,23 @@ class QuantConv_DW(QuantHelper):
         nn.init.kaiming_normal_(self.dw_weight)
         self.groups = prod_dw_in_planes #* in_planes
         self.conv = partial(F.conv2d, stride=stride, padding=padding, groups=self.groups)
+        # self.f_bn = nn.BatchNorm2d(in_planes)
         #print(f'dw inc{self.in_planes}; groups:{self.groups}')
 
     
     def normal_forward(self, x, *args):
         dw_weight = repeat(self.dw_weight, 'o i h w -> (o repeat) i h w', repeat=self.prod_dw_in_planes//self.in_planes)
         out = self.conv(x, dw_weight)
-        self.diff = 0
+        self.diff = torch.Tensor([0]).cuda()
         return out
     
     def quant_forward(self, x, quantizer):
         reshaped_w = rearrange(self.dw_weight, 'o i h w -> o i (h w)', h=self.kernel_size, w=self.kernel_size)
-        quantized_w, self.diff, idxs = quantizer(reshaped_w)
+        quantized_w, self.diff, _ = quantizer(reshaped_w)
         quantized_w = rearrange(quantized_w, 'o i (h w) -> o i h w', h=self.kernel_size, w=self.kernel_size)
         quantized_w = repeat(quantized_w, 'o i h w -> (o repeat) i h w', repeat=self.prod_dw_in_planes//self.in_planes)
         out = self.conv(x, quantized_w)
+        # out = self.f_bn(out)
         return out
     
     
@@ -102,13 +107,13 @@ class QuantConv_PW(QuantHelper):
         super().__init__()
         self.prod_dw_in_planes = prod_dw_in_planes
         self.in_planes = in_planes
-
+        self.use_quant = False
         self.out_planes = out_planes
         self.pw_weight = Parameter(torch.empty((out_planes*in_planes, 1, 1, 1)))
         nn.init.kaiming_normal_(self.pw_weight)
         self.groups = out_planes * prod_dw_in_planes
         self.conv = partial(F.conv2d, stride=stride, padding=padding, groups=self.groups)
-
+        # self.f_bn = nn.BatchNorm2d(out_planes)
         #print(f'pw inc{self.in_planes}; out_c {self.out_planes}; groups:{self.groups}')
 
     def get_groups(self):
@@ -119,19 +124,23 @@ class QuantConv_PW(QuantHelper):
         h, w = self.pw_weight.shape[-2:]
         pw_weight = repeat(self.pw_weight, 'o i h w -> o (repeat i) h w', repeat=self.prod_dw_in_planes//self.in_planes)
         pw_weight = rearrange(pw_weight, 'o i h w -> (o i) () h w', h=h, w=w)
+        # print('x', x)
+        # print('weight', pw_weight)
         out = self.conv(x, pw_weight)
-        self.diff = 0
+        # print('out', out)
+        self.diff = torch.Tensor([0]).cuda()
         return out
     
     def quant_forward(self, x, quantizer):
         x = repeat(x, 'b c h w -> b (repeat c) h w', repeat=self.out_planes)
         h, w = self.pw_weight.shape[-2:]
         reshaped_w = rearrange(self.pw_weight, 'o i h w -> o i (h w)')
-        quantized_w, self.diff, idxs = quantizer(reshaped_w)
+        quantized_w, self.diff, _ = quantizer(reshaped_w)
 
         quantized_w = repeat(quantized_w, 'o i hw -> o (repeat i) hw', repeat=self.prod_dw_in_planes//self.in_planes)
         quantized_w = rearrange(quantized_w, 'o i (h w) -> (o i) () h w', h=h, w=w)
         out = self.conv(x, quantized_w)
+        # out = self.f_bn(out)
         return out
 
 
@@ -149,7 +158,10 @@ class Quant_dwpw(QuantHelper):
         self.groups = self.pw_conv.get_groups()
         self.quantizer_dw = Quantizer(kernel_size ** 2, n_dw_emb)
         self.quantizer_pw = Quantizer(1, n_pw_emb)
-
+        self.n_fdw_emb = prod_dw_in_planes // 2 if prod_dw_in_planes//2 > self.n_f_emb else self.n_f_emb
+        self.n_fpw_emb = prod_dw_in_planes * out_planes // 2 if prod_dw_in_planes * out_planes // 2 > self.n_f_emb else self.n_f_emb
+        # self.f_bn1 = nn.BatchNorm2d(prod_dw_in_planes)
+        # self.f_bn2 = nn.BatchNorm2d(prod_dw_in_planes)
 
     def get_groups(self):
         return self.groups
@@ -158,8 +170,8 @@ class Quant_dwpw(QuantHelper):
         b, c, h1, w1 = x.shape
         # self.dw_conv(x, self.quantizer_dw)
         b, c, h2, w2 = self.dw_conv(x, self.quantizer_dw).shape
-        self.feat_quantizer_dw = FeatureQuantizer(h1*w1, self.n_f_emb, self.decay).to(x.device)
-        self.feat_quantizer_pw = FeatureQuantizer(h2*w2, self.n_f_emb, self.decay).to(x.device)
+        self.feat_quantizer_dw = FeatureQuantizer(h1*w1, self.n_fdw_emb, self.decay).to(x.device)
+        self.feat_quantizer_pw = FeatureQuantizer(h2*w2, self.n_fpw_emb, self.decay).to(x.device)
         
     def normal_forward(self, x):
         h_dw = self.dw_conv(x, self.quantizer_dw)
@@ -167,9 +179,13 @@ class Quant_dwpw(QuantHelper):
         return (h_pw, x) if self.ret_x else h_pw
             
     def quant_forward(self, x):
+        # h_dw = self.f_bn1(x)
         h_dw = self.feat_quantizer_dw(x)
+
         h_dw = self.dw_conv(h_dw, self.quantizer_dw)
+        # h_dw = self.f_bn2(h_dw)
         h_pw = self.feat_quantizer_pw(h_dw)
+
         h_pw = self.pw_conv(h_pw, self.quantizer_pw)
         return (h_pw, x) if self.ret_x else h_pw
     
@@ -215,38 +231,39 @@ class QuantNet(nn.Module):
             nn.ReLU(inplace=True))
         # the first dwpw layer has prod_dw_in_planes = 1
         self.conv2 = self._make_layer(block, out_planes, num_blocks[0], 1, n_dw_emb, n_pw_emb, gs, n_f_emb)
-        self.conv3 = self._make_layer(block, out_planes*2, num_blocks[1], 2, n_dw_emb, n_pw_emb, gs, n_f_emb)
+        # self.conv3 = self._make_layer(block, out_planes*2, num_blocks[1], 2, n_dw_emb, n_pw_emb, gs, n_f_emb)
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.head = nn.Linear(out_planes*2 * block.expansion, num_classes)
+        # self.head = nn.Linear(out_planes*2 * block.expansion, num_classes)
+        self.head = nn.Linear(out_planes * block.expansion, num_classes)
+        self._get_groups_pw_out_planes()
     # n_dw_emb, n_pw_emb, n_f_emb, in_planes, out_planes, stride=1, gs=1):
     def _make_layer(self, block, out_planes, num_blocks, stride, n_dw_emb, n_pw_emb, gs, n_f_emb):
         strides = [stride] + [1] * (num_blocks - 1)
         block_net = nn.Sequential()
         for i in range(len(strides)):
+
             block_net.add_module(f'QuantRes{i+1}', block(n_dw_emb, n_pw_emb, n_f_emb, self.prod_dw_in_planes, self.inplanes, out_planes, strides[i], gs))
             self.inplanes = out_planes * block.expansion
             self.prod_dw_in_planes = block_net[-1].get_prod_dw_in_planes()
-            # self.prod_dw_in_planes = self.inplanes * tmp_prod_dw_in_planes * self.prod_dw_in_planes
-            #todo the right order of the list
-            # self.prod_dw_in_planes_list.append(self.prod_dw_in_planes)
+
         return block_net
+
     def _get_groups_pw_out_planes(self):
         group_list = []
         for m in self.modules():
             if isinstance(m, QuantConv_PW):
                 group_list.append(m.out_planes)
-        return group_list
+        group_list.reverse()
+        self.group_list = group_list
 
     def forward(self, x):
         h = self.conv1(x)
         h = self.conv2(h)
-        h = self.conv3(h)
-        group_list = self._get_groups_pw_out_planes()
-        group_list.reverse()
         h = rearrange(h, 'b c h w -> b h w c')
-        h = h.view(*h.size()[:3], *group_list, self.out_planes)
+        # h = h.view(*h.size()[:3], *self.group_list, self.out_planes)
+        h = h.view(*h.size()[:3], *self.group_list, self.out_planes)
         h = h.sum(dim=-1)
-        for i in range(len(group_list), 1, -1):
+        for i in range(len(self.group_list), 1, -1):
             h = h.sum(dim=i+2)
         h = rearrange(h, 'b h w c-> b c h w')
         # h = rearrange(h, 'b (group c) h w ->  group c b h w ', group=group_list[-1])
@@ -255,8 +272,6 @@ class QuantNet(nn.Module):
         #     h = rearrange(tmp, '(group c) b h w ->  group c b h w ', group=group_list[len(group_list) - 2 - i])
         h = self.avg_pool(h)
         h = h.view(h.size(0), -1)
-
-
 
         h = self.head(h)
         return h
